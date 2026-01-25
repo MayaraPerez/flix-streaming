9c7685f39eb0ef01f201063e6f40c647f7e520d2
Mayara Perez <perezzmayara@gmail.com>
2026-01-25 17:28:27 -0300

    feat: Inclusion of functionality is styling
    [x] logo in the navbar
    [x] icon user form input

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,23 +1,46 @@
 import { Link } from "react-router-dom";
 import { handlerClick } from "../../utils/search";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../logo.png";
+
 import "./style.css";
 
 function NavBar() {
   return (
-    <div className="container-itens">
-      <div className="search" onClick={handlerClick}>
-        <div className="search-icon"></div>
+    <>
+      <div className="container">
+        <div className="container-logo">
+          <img src={logo} alt="logo" />
+        </div>
 
-        <input type="text" className="search-input" placeholder="Search..." />
+        <div className="container-itens">
+          <div className="search" onClick={handlerClick}>
+            <div className="search-icon"></div>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search..."
+            />
+          </div>
+
+          <Link className="link" to={"/"}>
+            <div className="item">Home</div>
+          </Link>
+          <Link className="link" to={"/favorites"}>
+            <div className="item">Favorite</div>
+          </Link>
+        </div>
+
+        <div className="container-user">
+          <Link to={`/user/`}>
+            <div className="myUser">
+              <FontAwesomeIcon className="icon" icon={faUser} />
+            </div>
+          </Link>
+        </div>
       </div>
-
-      <Link className="link" to={"/"}>
-        <div className="item">Home</div>
-      </Link>
-      <Link className="link" to={"/favorites"}>
-        <div className="item">Favorite</div>
-      </Link>
-    </div>
+    </>
   );
 }
 
