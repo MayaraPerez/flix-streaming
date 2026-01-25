6c68d98383812f1e12309f9ef4652196bf47701d
Mayara Perez <perezzmayara@gmail.com>
2026-01-25 17:33:15 -0300

    feat: Including a feature to delete movies from the favorites page is a styling addition.

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar";
@@ -11,30 +11,46 @@ function Favorite() {
     setFavoriteMovie(JSON.parse(favoriteFilm) || []);
   }, []);
 
-  function handlerDelete() {
-
+  function handlerDelete(id) {
+    const IdFavoritesFilme = favoriteMovie.filter((item) => item.id !== id )
+    
+    localStorage.setItem("@flixMovies", JSON.stringify(IdFavoritesFilme))
+    setFavoriteMovie(IdFavoritesFilme)
   }
   return (
     <>
       <NavBar />
-
+  
       <div className="container-favorite">
-        <h2>Meus Filmes</h2>
         <ul>
           {favoriteMovie.map((item) => (
-            <li key={item.id}>
+            
+            <li key={item.id} className="favorite-item">
+  
+              <div className="favoritePoster">
+                <img
+                  className="posterSelected"
+                  src={`http://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                  alt={item.title}
+                />
+              </div>
+  
               <span>{item.title}</span>
-
+  
               <div className="actions">
                 <Link to={`/movie/${item.id}`}>Ver Detalhes</Link>
-
-                <button onClick={() => handlerDelete(item.id)}>Remover</button>
+  
+                <button onClick={() => handlerDelete(item.id)}>
+                  Remover
+                </button>
               </div>
+  
             </li>
           ))}
         </ul>
       </div>
     </>
   );
+  
 }
 export default Favorite;
